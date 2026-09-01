78f0a948e711d51652bdcb485007778230928d59
DONGEUN_KANG <imduncan0826@gmail.com>
2026-09-01 17:37:46 +0800

    Add loop to print squares of numbers

---
+++ python/HackerRank/(5) Loops.py
@@ -0,0 +1,6 @@
+if __name__ == '__main__':
+    n = int(input())
+    
+    for i in range(n):
+        # Same result as i * i, but i ** 2 is more readable and clearly represents squaring.
+        print(i ** 2)
